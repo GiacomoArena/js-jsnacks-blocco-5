c6aadefb7192bea7314a2c3df96b702c65f73d7f
GiacomoArena <giacomoarena28@gmail.com>
2023-04-04 17:29:10 +0200

    update commit

--- jSnack 2/script.js
+++ jSnack 2/script.js
@@ -2,36 +2,60 @@ const studenti = [
   {
     name: "Giacomo",
     lastname: "Arena",
-    numMatricola: 24,
+    numMatricola: 348537,
+    voti:[4,8,7,6,7],
   },
   {
     name: "Giovanni",
     lastname: "Muciaccia",
-    numMatricola: 15,
+    numMatricola: 153745,
+    voti:[5,4,3,5,5],
   },
   {
     name: "Mario",
     lastname: "Rossi",
-    numMatricola: 13,
+    numMatricola: 137453,
+    voti:[4,8,7,6,7],
   },
   {
     name: "Fiorella",
     lastname: "Mannoia",
-    numMatricola: 69
+    numMatricola: 69347,
+    voti:[8,7,8,8,6],
   },
   {
     name: "Antonello",
     lastname: "Venditti",
-    numMatricola: 74,
+    numMatricola: 74754,
+    voti:[4,3,5,7,2],
   },
   {
     name: "Domenico",
     lastname: "Bini",
-    numMatricola: 11,
+    numMatricola: 134751,
+    voti:[5,6,7,9,4],
   },
   {
     name: "Claudio",
     lastname: "Baglioni",
-    numMatricola: 71,
+    numMatricola: 775141,
+    voti:[4,8,7,6,7],
   }
 ]
+
+studenti.forEach((element) => {
+  const mediaVoti = studenti.map((voto, i, voti)=> {
+    let media = 0
+    return   (media += element.voti[i])
+  })
+  console.log(mediaVoti);
+
+})
+
+  
+
+const datiUtente  =  studenti.map(studente => {
+  return `${studente.name} ${studente.lastname}`
+})
+
+console.log(datiUtente);
